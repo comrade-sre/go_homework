ee4cf7b1bd2a90ea7cb81e87e6369f51217e3930
Andrei Mironov <andrei.mironov@corp.mail.ru>
2020-11-30 20:43:41 +0300

    added calc

---
+++ calc.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func sub(x int, y int) int {
+	return x - y
+}
+func mult(a int, b int) int  {
+	return a * b
+}
+func div(a int, b int) int {
+	return a / b
+}
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	a,_ := strconv.Atoi(os.Args[1])
+	b,_ := strconv.Atoi(os.Args[2])
+switch op := os.Args[3]; op {
+	case "+":
+		fmt.Println(add(a, b))
+	case "-":
+		fmt.Println(sub(a, b))
+    case "*":	
+		fmt.Println(mult(a, b))
+	case "/":
+		fmt.Println(div(a, b))
+	}
+}
+
