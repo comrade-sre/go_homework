86d4443f330c10be24e12cdf9128c0530054f93e
Andrei Mironov <andrei.mironov@corp.mail.ru>
2020-11-30 20:45:28 +0300

    fotmatted  calc

--- calc.go
+++ calc.go
@@ -9,7 +9,7 @@ import (
 func sub(x int, y int) int {
 	return x - y
 }
-func mult(a int, b int) int  {
+func mult(a int, b int) int {
 	return a * b
 }
 func div(a int, b int) int {
@@ -21,17 +21,16 @@ func add(x int, y int) int {
 }
 
 func main() {
-	a,_ := strconv.Atoi(os.Args[1])
-	b,_ := strconv.Atoi(os.Args[2])
-switch op := os.Args[3]; op {
+	a, _ := strconv.Atoi(os.Args[1])
+	b, _ := strconv.Atoi(os.Args[2])
+	switch op := os.Args[3]; op {
 	case "+":
 		fmt.Println(add(a, b))
 	case "-":
 		fmt.Println(sub(a, b))
-    case "*":	
+	case "*":
 		fmt.Println(mult(a, b))
 	case "/":
 		fmt.Println(div(a, b))
 	}
 }
-
