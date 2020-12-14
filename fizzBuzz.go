045173d7b93fb743b7c78bd3d5f5c813cc9f15a5
Andrei Mironov <andrei.mironov@corp.mail.ru>
2020-12-14 14:07:26 +0300

    fibonacci

--- fizzBuzz.go
+++ fizzBuzz.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i < 101; i++ {
-		if i%15 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
-		} else if i%3 == 0 {
-			fmt.Println("Fizz")
-		} else {
-			fmt.Println(i)
-		}
-	}
+  for i := 1; i < 101; i++ {
+
+    switch {
+    case i%15 == 0:
+      fmt.Println("FizzBuzz ", i)
+    case i%5 == 0:
+      fmt.Println("Buzz ", i)
+    case i%3 == 0:
+      fmt.Println("Fizz ", i)
+    default:
+      fmt.Println(i)
+    }
+  }
 }
+
