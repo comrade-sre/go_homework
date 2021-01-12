482a6bf63bb5c799d1a5ab704786849dcc20b2bb
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-01-12 13:52:44 +0300

    added test for searchSimple

--- searchSimple/searchSimple.go
+++ searchSimple/searchSimple.go
@@ -1,22 +1,17 @@
 package searchSimple
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
-
 func search(x int) (result []int) {
 	stack := []int{2}
 	var isSimple bool
-	if x == 1 {
-		result = append(result, 1)
+	if x == 1  {
+		result = append(result, x)
 		return
 	}
+	result = append(result, 2)
 	for i := 3; i < x; i++ {
 		for _, j := range stack {
 			isSimple = true
-			if i%j == 0 {
+			if i%j == 0  {
 				stack = append(stack, i)
 				isSimple = false
 				break
@@ -30,15 +25,3 @@ func search(x int) (result []int) {
 	return result
 }
 
-//func main() {
-//	if len(os.Args) < 2 {
-//		fmt.Println("enter the number")
-//		os.Exit(1)
-//	}
-//	num, numErr := strconv.Atoi(os.Args[1])
-//	if numErr != nil || num < 1 {
-//		fmt.Println("enter positive number")
-//		os.Exit(1)
-//	}
-//	fmt.Println(search(num))
-//}
