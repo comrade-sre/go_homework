e4c5ccb5161c6bac40fa7ea8c62f0bc1d8a06747
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-01-12 11:32:19 +0300

    fix fib test

--- fibonacci/fibonacci.go
+++ fibonacci/fibonacci.go
@@ -1,16 +1,9 @@
 package fibonacci
 
-import (
-	"fmt"
-	"os"
-	"runtime"
-	"strconv"
-	"strings"
-)
 
-var calculated = make(map[int]int)
 
-func fibonacci(number int) int {
+func Fibonacci(number int) int {
+	var calculated = make(map[int]int)
 	if number <= 1 {
 		return number
 	} else if val, ok := calculated[number]; ok {
@@ -21,18 +14,3 @@ func fibonacci(number int) int {
 		return result
 	}
 }
-//func main() {
-//	_, file, _, _ := runtime.Caller(1)
-//	if len(os.Args) < 2 {
-//		scriptName := strings.Split(file, "/")
-//		fmt.Printf("usage: %s int positive number\n", scriptName[len(scriptName)-1])
-//		os.Exit(1)
-//	}
-//	var number int
-//	number, numErr := strconv.Atoi(os.Args[1])
-//	if numErr != nil {
-//		fmt.Printf("%s is incorrect number\n", os.Args[1])
-//		os.Exit(1)
-//	}
-//	fmt.Println(fibonacci(number))
-//}
