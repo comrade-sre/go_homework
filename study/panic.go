e273926b0d577d4ecdcabd30a935f508cc629e77
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-02-23 10:52:21 +0300

    fixed

--- study/panic.go
+++ study/panic.go
@@ -2,27 +2,18 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"time"
 	"fmt"
 	"os"
 )
 
 func main() {
-	var err error
-	err = NewError("custom error")
-	fmt.Println(err)
 	f, err := os.OpenFile("test", os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 	defer f.Close()
-	defer func() {
-		if v := recover(); v != nil {
-			log.Println("capture a panic:", v)
-			log.Println("avoid crashing the program")
-		}
-	}()
 	intentPanic()
 	
 }
@@ -38,13 +29,16 @@ func NewError(text string) error {
 		date:    time.Now().Format(time.RFC850),
 	}
 }
+
 func (e *ErrorDate) Error() string {
 	return fmt.Sprintf("error: %s\t%s", e.date, e.message)
 }
-func intentPanic() {
-	var i int
-	for i = 1000; i > -1; i-- {
-		println(rand.Intn(1000) / i)
 
-	}
+func intentPanic() {
+	defer func() {
+		if v := recover(); v != nil {
+			log.Println("capture a panic:", v)
+		}
+	}()
+	panic(NewError("custom error"))
 }
