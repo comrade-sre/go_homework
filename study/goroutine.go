13d9c14b0539506438d6ad7e8844cacbf98f76cb
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-03-06 00:05:53 +0300

    bla-bla

--- study/goroutine.go
+++ study/goroutine.go
@@ -3,15 +3,19 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
 	var num int
-	for i := 0; i < 1001; i++ {
+	var mutex sync.Mutex
+	for i := 0; i < 1000; i++ {
 		go func() {
+			mutex.Lock()
 			num++
+			mutex.Unlock()
 		}()
 	}
-	time.Sleep(10 * time.Second)
+	time.Sleep(3 * time.Second)
 	fmt.Println(num)
 }
