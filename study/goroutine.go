ab9cc9e535e815cea3c99a51ac1ae61eb4b1cdee
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-03-11 12:22:12 +0300

    added trace

--- study/goroutine.go
+++ study/goroutine.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"runtime/trace"
 	"sync"
+	"time"
 )
 
 func main() {
+	trace.Start(os.Stderr)
+	defer trace.Stop()
+
 	var num int
 	var mutex sync.Mutex
 	for i := 0; i < 1000; i++ {
