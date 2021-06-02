8dc35591076a55161cfbc209c0dd146bc77e8fea
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-06-02 19:15:13 +0300

    added logging for seekDup

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/comrade-sre/go_homework
 
 go 1.15
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
--- study/seekDupFin.go
+++ study/seekDupFin.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"os"
 	"sync"
+	"go.uber.org/zap"
 )
 
 var (
@@ -41,14 +42,19 @@ func (s *Set) Has(i [32]uint8) bool {
 	return ok
 }
 func (s *Set) Del(i [32]uint8) {
-	//	s.Lock()
 	delete(s.res, i)
-	//	s.Unlock()
 }
 func main() {
 	flag.Parse()
+	host,_ := os.Hostname()
+	logger, err := NewLogger()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	defer logger.Sync()
+	logger = logger.With(zap.String("hostname: ", host))
 	var result = NewSet()
-	GetFiles(*root)
+	GetFiles(*root, logger)
 	go func() {
 		for _, path := range paths {
 			ch <- path
@@ -57,16 +63,23 @@ func main() {
 	}()
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go worker(ch, result)
+		go worker(ch, result, logger)
 	}
 	wg.Wait()
 	paths = nil
-	deDup(result)
+	deDup(result, logger)
 
+}
+func NewLogger() (*zap.Logger, error) {
+  cfg := zap.NewProductionConfig()
+  cfg.OutputPaths = []string{
+    "/Users/andrei.mironov/test/test.log",
+  }
+  return cfg.Build()
 }
 
 // function for deleting duplicates from map
-func deDup(result *Set) {
+func deDup(result *Set, logger *zap.Logger) {
 	for hash, paths := range result.res {
 		length := len(paths)
 		if length > 1 {
@@ -74,7 +87,7 @@ func deDup(result *Set) {
 				if *del {
 					err := os.Remove(paths[i])
 					if err != nil {
-						fmt.Fprintf(os.Stdout, "cannot delete file %s due to %v\n", paths[i], err)
+						logger.With(zap.Uint64("uid", 00007)).Error("cannot delete file " + paths[i] + " due to" + err.Error())
 					}
 					fmt.Fprintf(os.Stdout, "%s deleted\n", paths[i])
 				} else {
@@ -85,39 +98,41 @@ func deDup(result *Set) {
 		result.Del(hash)
 	}
 }
-func worker(ch chan string, result *Set) {
+func worker(ch chan string, result *Set, logger *zap.Logger) {
 	for path := range ch {
-		CompareFiles(path, result)
+		CalcHash(path, result, logger)
 	}
 	wg.Done()
 }
 
 // function GetFiles for getting all files from root directory recursively
-func GetFiles(root string) (files []os.FileInfo) {
+func GetFiles(root string, logger *zap.Logger) (files []os.FileInfo) {
 	files, err := ioutil.ReadDir(root)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		logger.With(zap.Uint64("uid", 00017)).Error(err.Error())
 	}
 	for _, file := range files {
 		if file.IsDir() {
-			GetFiles(root + "/" + file.Name())
+			GetFiles(root + "/" + file.Name(), logger)
 		} else if file.Mode().IsRegular() {
-			paths = append(paths, root+"/"+file.Name())
+			paths = append(paths, root+file.Name())
+			logger.With(zap.Uint64("uid", 00001)).Info("file successfully added " + root+file.Name())
 		} else {
-			fmt.Fprintf(os.Stderr, "%s is not a regular file\n", root+"/"+file.Name())
+			logger.With(zap.Uint64("uid", 00005)).Error("file " + root+file.Name() + " is not a regular file")
 		}
 	}
 	return
 }
 
 // function compareFiles function to compare files by sha256summ and add to result map
-func CompareFiles(path string, result *Set) {
+func CalcHash(path string, result *Set, logger *zap.Logger) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		logger.With(zap.Uint64("uid", 00006)).Error("cannot open " + path)
 		return
-	}
+	} 
 	hash := sha256.Sum256([]byte(data))
 	result.Add(hash, path)
+	logger.With(zap.Uint64("uid", 00002)).Info("hash for " + path + " was added")
 	return
 }
