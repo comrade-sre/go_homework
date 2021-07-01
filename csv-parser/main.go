37bb24d25c1576b45409d1a49a1da6e4e9764858
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-07-01 22:14:27 +0300

    added syntax parser

--- csv-parser/main.go
+++ csv-parser/main.go
@@ -2,44 +2,28 @@ package main
 
 import (
 	"bufio"
-	"errors"
+	//"errors"
 	"flag"
 	"fmt"
 	"github.com/comrade-sre/go_homework/csv-parser/log"
 	"github.com/comrade-sre/go_homework/csv-parser/parse"
 	"io"
 	"os"
+	"strconv"
 	"strings"
+	//"reflect"
 )
 
-type Expression []string
-
-func (exp *Expression) String() string {
-	var buff strings.Builder
-	buff.Grow(32)
-	for _, val := range *exp {
-		fmt.Fprintf(&buff, "%s", val)
-	}
-	return buff.String()
-
-}
-func (exp *Expression) Set(value string) error {
-	*exp = append(*exp, value)
-	return nil
-
-}
-
 var (
-	config  = flag.String("c", "./config.yaml", "path to the configuration file")
-	sigChan = make(chan os.Signal, 1)
-	query   Expression
+	config    = flag.String("c", "./config.yaml", "path to the configuration file")
+	sigChan   = make(chan os.Signal, 1)
+	Header    []string
+	IsString []bool
 )
 
 func main() {
-	flag.Var(&query, "", "query string")
 	flag.Parse()
-	fmt.Println(*query)
-
+	Expression := flag.Args()
 	configFile, err := os.OpenFile(*config, os.O_RDONLY, 0666)
 	if err != nil {
 		panic(err.Error())
@@ -65,27 +49,40 @@ func main() {
 	LineChan := make(chan string, 100)
 	reader := bufio.NewReader(csv)
 	RawHeader, _, err := reader.ReadLine()
-	Header := (strings.Split(string(RawHeader), ","))
+	FirstDataLineRaw, _, err := reader.ReadLine()
+    FirstDataLine := strings.Split(string(FirstDataLineRaw), ",")
+	Header = (strings.Split(string(RawHeader), ","))
+
+
+    IsString = GetTypes(Header, FirstDataLine)
+	fmt.Println(IsString)
 	go ReadCsv(*reader, LineChan)
-	column := "Country/Region"
-	pattern := "US"
-	index, err := CheckHeader(Header, column)
+	err = parse.ParseQuerySyntax(Header, Expression)
+	if err != nil {
+		panic(err.Error())
+	}
+	col := "Country/Region"
+	index, err := parse.CheckHeader(Header, col)
+	pattern := "China"
 	if err != nil {
 		panic(err.Error())
 	}
 	for i := 0; i < 10; i++ {
-		ParseLine(index, pattern, LineChan)
+		parse.ParseLine(index, pattern, LineChan)
 	}
 
 }
-func CheckHeader(header []string, column string) (index int, err error) {
-	for index, value := range header {
-		if value == column {
-			return index, nil
+
+func GetTypes(header []string, FirstDataLine []string) (IsString []bool) {
+    for _, field := range FirstDataLine {
+	    _, err := strconv.ParseFloat(field, 32)
+		if err != nil {
+			IsString = append(IsString,true)
+		} else {
+			IsString = append(IsString, false)
 		}
 	}
-	err = errors.New("such column not found")
-	return 0, err
+	return IsString
 }
 
 func ReadCsv(reader bufio.Reader, ch chan string) (err error) {
@@ -99,14 +96,5 @@ func ReadCsv(reader bufio.Reader, ch chan string) (err error) {
 		ch <- string(line)
 	}
 	close(ch)
-	return nil
-}
-func ParseLine(index int, pattern string, ch <-chan string) (err error) {
-	for line := range ch {
-		values := strings.Split(line, ",")
-		if values[index] == pattern {
-			fmt.Println(line)
-		}
-	}
 	return nil
 }
--- csv-parser/parse/parse.go
+++ csv-parser/parse/parse.go
@@ -3,6 +3,9 @@ package parse
 import (
 	"gopkg.in/yaml.v2"
 	"io"
+	"fmt"
+	"strings"
+	//"errors"
 )
 
 type ConfType struct {
@@ -15,4 +18,55 @@ func Parse(file io.Reader) (appConf ConfType, err error) {
 	appConf = ConfType{}
 	err = yaml.NewDecoder(file).Decode(&appConf)
 	return appConf, err
+}
+func ParseLine(index int, pattern string, ch <-chan string) (err error) {
+	for line := range ch {
+		values := strings.Split(line, ",")
+		if values[index] == pattern {
+			fmt.Println(line)
+		}
+	}
+	return nil
+}
+func CheckHeader(header []string, column string) (index int, err error) {
+	for index, value := range header {
+		if value == column {
+			return index, nil
+		}
+	}
+	return 0, fmt.Errorf("such column not found: %s", column)
+
+}
+func ParseQuerySyntax(header []string, query []string) (err error) {
+    HeaderLength := len(header)
+    LogOpMax := HeaderLength - 1
+    QearyMax := (HeaderLength * 3) + LogOpMax
+    Querylength := len(query)
+    if Querylength > QearyMax {
+        return fmt.Errorf("query is too long, max length: %d", QearyMax)
+
+    }
+    for i := 0; i < Querylength; i+=4 {
+        if _, err = CheckHeader(header, query[i]); err != nil {
+        return err
+        }
+    }
+    for i := 1; i < Querylength; i+=4 {
+        op := query[i]
+        if  op != ">" && op != "<" && op != "=" {
+
+            return fmt.Errorf("%s is inappropriate operator(it should be ><=)", op)
+        }
+    for i := 3; i < Querylength; i+=4 {
+        op := strings.ToLower(query[i])
+        if op != "and" && op != "or" {
+        return fmt.Errorf("%s is inappropriate logical operator(it should be and/or)", op)
+        }
+    }
+    }
+
+
+
+    return nil
+
 }
