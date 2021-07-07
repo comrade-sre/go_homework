db6e6bd35368f825b4b8866c31ea9750c443c3ad
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-07-07 18:45:29 +0300

    rename test function

--- csv-parser/parse/parse_test.go
+++ csv-parser/parse/parse_test.go
@@ -6,10 +6,11 @@ import (
 )
 
 var (
-     QueryEqual = []string{"us","uS","3242","huy","23.423","23.423","China","cHina","234", "234.0","1/8/2020","01/08/2020"}
+     QueryEqual = []string{"us","uS","3242","3242","23.423","23.423","China","cHina","234", "234.0","1/8/2020","01/08/2020"}
+
 )
 
-func TestEqualCopmare(t *testing.T) {
+func TestCompareValues(t *testing.T) {
     for i := 0; i == len(QueryEqual)-3;i +=2 {
         fmt.Println(QueryEqual[i], QueryEqual[i+1])
         result := CompareValues(QueryEqual[i], QueryEqual[i+1], "=")
