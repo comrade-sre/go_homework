2f87b09773c07c61d1a69b756bd34a34f81cd44d
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-07-07 18:54:30 +0300

    replace slice by map

--- csv-parser/parse/parse_test.go
+++ csv-parser/parse/parse_test.go
@@ -1,21 +1,19 @@
 package parse
 import (
     "testing"
-    "fmt"
 
 )
 
 var (
-     QueryEqual = []string{"us","uS","3242","3242","23.423","23.423","China","cHina","234", "234.0","1/8/2020","01/08/2020"}
+     QueryEqual = map[string]string{"us": "us","3242": "3242","23.423": "23.423","China": "China","234": "234.0","1/8/2020": "01/08/2020"}
 
 )
 
 func TestCompareValues(t *testing.T) {
-    for i := 0; i == len(QueryEqual)-3;i +=2 {
-        fmt.Println(QueryEqual[i], QueryEqual[i+1])
-        result := CompareValues(QueryEqual[i], QueryEqual[i+1], "=")
+    for key, val := range QueryEqual {
+        result := CompareValues(key, val, "=")
         if result != true {
-            t.Errorf("gets false, wants true, values: %s = %s", QueryEqual[i], QueryEqual[i+1])
+            t.Errorf("gets false, wants true, values: %s = %s", key, val)
         }
     }
 
