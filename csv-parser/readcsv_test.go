fee681d73d46810c4d03e7ba39b11a0ae50d8d62
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-07-08 18:22:25 +0300

    added print mem usage for read csv test

--- csv-parser/readcsv_test.go
+++ csv-parser/readcsv_test.go
@@ -3,14 +3,16 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"go.uber.org/zap"
 	"math/rand"
+	"runtime"
 	"testing"
 	"time"
 )
 
 const (
-	TestLength  = 100000
+	TestLength  = 1000000
 	letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\n"
 )
 
@@ -18,6 +20,11 @@ var (
 	ch = make(chan string, TestLength)
 )
 
+func PrintMemUsage() string {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	return fmt.Sprintf("Alloc = %v MiB", m.Alloc/1024/1024)
+}
 func RandBytes(n int) []byte {
 	b := make([]byte, n)
 	for i := range b {
@@ -33,7 +40,9 @@ func TestReadCsv(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
+	t.Log(PrintMemUsage())
 	data := RandBytes(TestLength)
+	PrintMemUsage()
 	buf := bytes.NewBuffer(data)
 	reader := bufio.NewReader(buf)
 	go ReadCsv(*reader, ch, logger)
@@ -41,6 +50,7 @@ func TestReadCsv(t *testing.T) {
 	for res := range ch {
 		i++
 		t.Logf("received random line  from channel: %s\n", res)
+		t.Log(PrintMemUsage())
 	}
 	if i == TestLength/15 {
 		t.Errorf("not all lines received from channel, wants %d, get %d", TestLength/15, i)
