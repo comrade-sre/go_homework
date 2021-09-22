b011b6e2c5523f579b21ae170ca35e8a8224e5e1
Andrei Mironov <andrei.mironov@corp.mail.ru>
2021-09-22 13:17:04 +0300

    added dummy http server

---
+++ http/dummy_server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Handler struct {
+}
+type Employee struct {
+	Name   string  `json:"name" xml:"name"`
+	Age    int     `json:"age" xml:"age"`
+	Salary float32 `json:"salary" xml:"salary"`
+}
+type UploadHandler struct {
+	UploadDir string
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		name := r.FormValue("name")
+		fmt.Fprintf(w, "Parsed param name with value: %s", name)
+	case http.MethodPost:
+		defer r.Body.Close()
+		contentType := r.Header.Get("Content-Type")
+		var employee Employee
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&employee)
+			if err != nil {
+				http.Error(w, "Unable to unmarshall json", http.StatusBadRequest)
+				return
+			}
+
+		case "application/xml":
+			err := xml.NewDecoder(r.Body).Decode(&employee)
+			if err != nil {
+				http.Error(w, "Unable to unmarshal XML", http.StatusBadRequest)
+				return
+			}
+		default:
+			http.Error(w, "Unknown content type", http.StatusBadRequest)
+			return
+
+		}
+		fmt.Fprintf(w, "Got a new employee\nName: %s\nAge: %d\nSalary: %0.2f\n",
+			employee.Name,
+			employee.Age,
+			employee.Salary,
+		)
+
+	}
+}
+func (h *UploadHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to read file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Unable to read file", http.StatusBadRequest)
+		return
+	}
+	filepath := h.UploadDir + "/" + header.Filename
+	err = ioutil.WriteFile(filepath, data, 0755)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Unable to save file", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "File %s successfully uploaded", header.Filename)
+}
+
+func main() {
+	handler := &Handler{}
+	uploadHandler := &UploadHandler{
+		UploadDir: "upload",
+	}
+	http.Handle("/upload", uploadHandler)
+	http.Handle("/", handler)
+	srv := &http.Server{
+		Addr:         ":80",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	srv.ListenAndServe()
+}
